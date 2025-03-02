9cfc2c34b1fa9b69354679fd27dac4f26231fcf9
am205379@gmail.com <am205379@gmail.com>
2025-03-02 15:49:37 +0330

    Append ThreadQueue method

--- main.cpp
+++ main.cpp
@@ -1,163 +1,185 @@
 #include <iostream>
-#include <queue>
+#include <fstream>
+#include <sstream>
 #include <vector>
-#include <pthread.h>
-#include <unistd.h>
+#include <queue>
+#include <thread>
+#include <atomic>
+#include <condition_variable>
+#include <chrono>
+#include <stdexcept>
+#include <algorithm>
 
 using namespace std;
 
-bool HoldAndWait(bool* lock) {
-    bool old_value = *lock;
-    *lock = true;
-    return old_value;
-}
-
 class Mutex {
 private:
-    bool locked;
+    atomic<bool> locked;
 
 public:
-    Mutex() : locked(false) {}
+    Mutex() : locked(false) {
+        //
+    }
 
-    void lock() {
-        while (HoldAndWait(&locked)) {
-            // Waitting For True Locked
+    void lock() {  
+        while (locked.exchange(true, memory_order_acquire)) {
+            this_thread::yield();  // Spinlock
         }
     }
 
-    void unlock() {
-        locked = false;
+    void unlock() {  
+        locked.store(false, memory_order_release);
+    }
+};
+
+template <typename T>
+class ThreadQueue {
+private:
+    std::queue<T> queue;
+    Mutex mutex;
+    condition_variable_any cvNotEmpty;
+    condition_variable_any cvNotFull;
+    size_t maxSize;
+
+public:
+    ThreadQueue(size_t size) : maxSize(size) {}
+
+    void Push(const T& item) {
+        mutex.lock();
+
+        while (queue.size() >= maxSize) {
+            cvNotFull.wait(mutex);
+        }
+
+        queue.push(item);
+        cvNotEmpty.notify_one();
+
+        mutex.unlock();
+    }
+
+    T Pop() {
+        mutex.lock();
+
+        while (queue.empty()) {
+            cvNotEmpty.wait(mutex);
+        }
+
+        T item = queue.front();
+
+        queue.pop();
+        cvNotFull.notify_one();
+        mutex.unlock();
+
+        return item;
+    }
+
+    bool IsEmpty() {
+        mutex.lock();
+        bool empty = queue.empty();
+        mutex.unlock();
+
+        return empty;
     }
 };
 
 struct Task {
     int id;
-    int start_time;
-    int end_time;
-    int execution_time;
+    int burstTime;
+    int arrivalTime;
 };
 
 class ThreadPool {
-    private:
-        queue<Task> task_queue;
-        vector<Task> completed_tasks;
-        vector<pthread_t> threads;
-        
-        Mutex queue_mutex;
-        pthread_mutex_t cond_mutex;
-        pthread_cond_t cond_var;
+private:
+    ThreadQueue<Task> taskQueue;
+    vector<thread> workers;
+    atomic<bool> stop;
 
-        bool stop;
-        int global_time;
-        int num_threads;
-
-    public:
-        ThreadPool(int num_threads) : stop(false), global_time(0), num_threads(num_threads) {
-            pthread_mutex_init(&cond_mutex, nullptr);
-            pthread_cond_init(&cond_var, nullptr);
-        }
-
-        ~ThreadPool() {
-            pthread_mutex_destroy(&cond_mutex);
-            pthread_cond_destroy(&cond_var);
-        }
-
-        void addTask(Task task) {
-            queue_mutex.lock();
-            task_queue.push(task);
-            queue_mutex.unlock();
-
-            pthread_cond_signal(&cond_var);
-        }
-
-        static void* workerStatic(void* arg) {
-            ThreadPool* pool = static_cast<ThreadPool*>(arg);
-            pool->worker();
-            return nullptr;
-        }
-
-        void worker() {
-            while (true) {
-                pthread_mutex_lock(&cond_mutex);
-
-                while (task_queue.empty() && !stop) {
-                    pthread_cond_wait(&cond_var, &cond_mutex);
-                }
-
-                if (stop && task_queue.empty()) {
-                    pthread_mutex_unlock(&cond_mutex);
-                    break;
-                }
-
-                queue_mutex.lock();
-                Task task = task_queue.front();
-                task_queue.pop();
-                queue_mutex.unlock();
-
-                pthread_mutex_unlock(&cond_mutex);
-
-                sleep(task.execution_time);
-
-                queue_mutex.lock();
-                task.start_time = global_time;
-                global_time += task.execution_time;
-                task.end_time = global_time;
-                completed_tasks.push_back(task);
-                queue_mutex.unlock();
+    void Worker(int id) {
+        while (!stop) {
+            Task task;
+            try {
+                task = taskQueue.Pop();
+            } catch (const runtime_error&) {
+                continue;
             }
+
+            cout << "Worker " << id << " Started Task "  << task.id
+                      << " (Arrival Time " << task.arrivalTime << "s)\n";
+
+            cout << "Worker " << id << " Finished Task " << task.id
+                      << " (Burst Time "   << task.burstTime << "s)\n";
         }
-
-        void startThreads() {
-            for (int i = 0; i < num_threads; ++i) {
-                pthread_t thread;
-                pthread_create(&thread, nullptr, workerStatic, this);
-                threads.push_back(thread);
-            }
-        }
-
-        void stopThreads() {
-            stop = true;
-            pthread_cond_broadcast(&cond_var);
-
-            for (auto& thread : threads) {
-                pthread_join(thread, nullptr);
-            }
-        }
-
-        void report() {
-            cout << "Simulation Result\n";
-            cout << "----------------\n";
-            if (!completed_tasks.empty()) {
-                cout << "Total Simulation Time: " << completed_tasks.back().end_time << " Seconds\n";
-            } else {
-                cout << "Total Simulation Time: 0 Seconds\n";
-            }
-            cout << "Number Of Completed Tasks: " << completed_tasks.size() << "\n";
-
-            for (const auto& task : completed_tasks) {
-                cout << "- Task " << task.id << ": Start=" << task.start_time << "s, End=" << task.end_time << "s\n";
-            }
-        }
-};
-
-int main() {
-    vector<Task> tasks = {
-        {0, 0, 0, 3},
-        {1, 0, 0, 4},
-        {2, 0, 0, 7},
-        {3, 0, 0, 5},
-        {4, 0, 0, 6}
-    };
-
-    ThreadPool pool(3);
-    pool.startThreads();
-
-    for (const auto& task : tasks) {
-        pool.addTask(task);
+        exit(1);
     }
 
-    pool.stopThreads();
-    pool.report();
+public:
+    ThreadPool(int workerCount, int queue_size)
+        : taskQueue(queue_size), stop(false) {
+        for (int i = 0; i < workerCount; ++i) {
+            workers.emplace_back(&ThreadPool::Worker, this, i);
+        } 
+    }
+
+    ~ThreadPool() {
+        stop = true;
+
+        for (auto& worker : workers) {
+            if (worker.joinable()) {
+                worker.join();
+            }
+        }
+    }
+
+    void AddTask(const Task& task) {
+        taskQueue.Push(task);
+    }
+};
+
+void FileReader(ThreadPool& pool, const string& fileName) {
+    ifstream file(fileName);
+
+    if (!file) {
+        cerr << "File Cannot Be Open!" << fileName << "\n";
+        return;
+    }
+
+    vector<Task> tasks;
+    string line;
+    int counterId = 1;
+
+    while (getline(file, line)) {
+        istringstream iss(line);
+        int arrival, burst;
+
+        if (!(iss >> arrival >> burst)) {
+            continue;
+        }
+
+        tasks.push_back({counterId++, burst, arrival});
+    }
+
+    file.close();
+
+    sort(tasks.begin(), tasks.end(), [](const Task& a, const Task& b) {
+        return a.arrivalTime < b.arrivalTime;
+    });
+
+    auto startTime = chrono::steady_clock::now();
+
+    for (const auto& task : tasks) {
+        this_thread::sleep_until(startTime + chrono::seconds(task.arrivalTime));
+        pool.AddTask(task);
+    }
+}
+
+int main() {
+    int workerCount = 9;      
+    int qeueSize    = 9;       
+
+    string fileName = "Test1.txt";  
+
+    ThreadPool pool(workerCount, qeueSize);
+    FileReader(pool, fileName);
 
     return 0;
 }
