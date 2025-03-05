e633e290326c35aa507f3446fae3c90b50a3a030
AM205379 <am205379@gmail.com>
2025-03-05 17:57:20 +0330

    Append coutLock property to class

--- main.cpp
+++ main.cpp
@@ -5,7 +5,6 @@
 #include <queue>
 #include <thread>
 #include <atomic>
-#include <condition_variable>
 #include <algorithm>
 
 using namespace std;
@@ -15,17 +14,15 @@ private:
     atomic<bool> locked;
 
 public:
-    Mutex() : locked(false) {
-        //
-    }
+    Mutex() : locked(false) {}
 
-    void lock() {  
+    void lock() {
         while (locked.exchange(true, memory_order_acquire)) {
-            this_thread::yield();  // Spinlock
+            this_thread::yield();
         }
     }
 
-    void unlock() {  
+    void unlock() {
         locked.store(false, memory_order_release);
     }
 };
@@ -33,49 +30,44 @@ public:
 template <typename T>
 class ThreadQueue {
 private:
-    std::queue<T> queue;
+    queue<T> queue;
     Mutex mutex;
-    condition_variable_any cvNotEmpty;
-    condition_variable_any cvNotFull;
     size_t maxSize;
 
 public:
     ThreadQueue(size_t size) : maxSize(size) {}
 
     void Push(const T& item) {
-        mutex.lock();
-
-        while (queue.size() >= maxSize) {
-            //
+        while (true) {
+            mutex.lock();
+            if (queue.size() < maxSize) {
+                queue.push(item);
+                mutex.unlock();
+                return;
+            }
+            mutex.unlock();
+            this_thread::yield();
         }
-
-        queue.push(item);
-        cvNotEmpty.notify_one();
-
-        mutex.unlock();
     }
 
     T Pop() {
-        mutex.lock();
-
-        while (queue.empty()) {
-            cvNotEmpty.wait(mutex);
+        while (true) {
+            mutex.lock();
+            if (!queue.empty()) {
+                T item = queue.front();
+                queue.pop();
+                mutex.unlock();
+                return item;
+            }
+            mutex.unlock();
+            this_thread::yield();
         }
-
-        T item = queue.front();
-
-        queue.pop();
-        cvNotFull.notify_one();
-        mutex.unlock();
-
-        return item;
     }
 
     bool IsEmpty() {
         mutex.lock();
         bool empty = queue.empty();
         mutex.unlock();
-
         return empty;
     }
 };
@@ -91,41 +83,51 @@ private:
     ThreadQueue<Task> taskQueue;
     vector<thread> workers;
     atomic<bool> stop;
+    atomic<int> activeWorkers;
+    atomic<bool> coutLock{false};
+
+    void SafePrint(const string& message) {
+        while (coutLock.exchange(true, memory_order_acquire)) {
+            this_thread::yield();
+        }
+        cout << message << endl;
+        coutLock.store(false, memory_order_release);
+    }
 
     void Worker(int id) {
-        while (!stop) {
-            Task task;
-            try {
-                task = taskQueue.Pop();
-            } catch (const runtime_error&) {
-                continue;
+        while (!stop || !taskQueue.IsEmpty()) {
+            if (!taskQueue.IsEmpty()) {
+                Task task = taskQueue.Pop();
+                SafePrint("Worker " + to_string(id) + " Started Task " + to_string(task.id) +
+                          " <Arrival Time " + to_string(task.arrivalTime) + "s>");
+
+                this_thread::sleep_for(chrono::seconds(task.burstTime));
+
+                SafePrint("Worker " + to_string(id) + " Finished Task " + to_string(task.id) +
+                          " <Execution Time " + to_string(task.burstTime) + "s>");
+            } else {
+                this_thread::yield();
             }
-
-            cout << "Worker " << id << " Started Task "  << task.id
-                      << " <Arrival Time " << task.arrivalTime << "s>\n";
-
-            cout << "Worker " << id << " Finished Task " << task.id
-                      << " <Excution Time "   << task.burstTime << "s>\n";
         }
-        exit(1);
+        activeWorkers.fetch_sub(1, memory_order_relaxed);
     }
 
 public:
     ThreadPool(int workerCount, int queue_size)
-        : taskQueue(queue_size), stop(false) {
+        : taskQueue(queue_size), stop(false), activeWorkers(workerCount) {
         for (int i = 0; i < workerCount; ++i) {
             workers.emplace_back(&ThreadPool::Worker, this, i);
-        } 
+        }
     }
 
     ~ThreadPool() {
         stop = true;
-
         for (auto& worker : workers) {
             if (worker.joinable()) {
                 worker.join();
             }
         }
+        SafePrint("Total workers used: " + to_string(workers.size()));
     }
 
     void AddTask(const Task& task) {
@@ -137,7 +139,7 @@ void FileReader(ThreadPool& pool, const string& fileName) {
     ifstream file(fileName);
 
     if (!file) {
-        cerr << "File Cannot Be Open!" << fileName << "\n";
+        cerr << "File Cannot Be Open! " << fileName << "\n";
         return;
     }
 
@@ -148,11 +150,9 @@ void FileReader(ThreadPool& pool, const string& fileName) {
     while (getline(file, line)) {
         istringstream iss(line);
         int arrival, burst;
-
         if (!(iss >> arrival >> burst)) {
             continue;
         }
-
         tasks.push_back({counterId++, burst, arrival});
     }
 
@@ -171,12 +171,11 @@ void FileReader(ThreadPool& pool, const string& fileName) {
 }
 
 int main() {
-    int workerCount = 3;      
-    int qeueSize    = 4;       
+    int workerCount = 3;
+    int queueSize = 4;
+    string fileName = "Task.txt";
 
-    string fileName = "Task.txt";  
-
-    ThreadPool pool(workerCount, qeueSize);
+    ThreadPool pool(workerCount, queueSize);
     FileReader(pool, fileName);
 
     return 0;
